745aaae5d0742c0232da6922b8bc52683c6ed9ce
lyndonjeff <jefflyndonebue@gmail.com>
2023-11-09 23:48:50 +0800

    may preprocess
    preprocesss

--- emcrypt.py
+++ emcrypt.py
@@ -1,12 +1,132 @@
-
 import os
+import re
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from textblob import TextBlob 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
+
+# Function to clean and preprocess text
+def preprocess_text(text):
+    # Remove emojis
+    text = re.sub(r"(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))", " ", text)
+    
+    # Remove special characters and punctuation
+    text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+    
+    # Convert text to lowercase
+    text = text.lower()
+    
+    # Tokenize the text
+    words = word_tokenize(text)
+    
+    # Remove stop words
+    stop_words = set(stopwords.words("english"))
+    filtered_words = [word for word in words if word not in stop_words]
+    
+    # Remove repeating characters (e.g., loooove -> love)
+    text = re.sub(r"(.)\1{2,}", r"\1", " ".join(filtered_words))
+    
+    return text
+
+def detect_emojis(text):
+    emoji_pattern = re.compile("["
+        u"\U0001F4B0"  # Money Bag
+        u"\U0001F4B5"  # Banknote with Dollar Sign
+        u"\U0001F4B8"  # Money with Wings
+        u"\U0001F4B9"  # Credit Card
+        u"\U0001F4B1"  # Currency Exchange
+        u"\U0001F4F1"  # Mobile Phone with Dollar Sign
+        u"\U0001F4F2"  # Personal Computer with Dollar Sign
+        u"\U0001F4F0"  # Money Mouth Face
+        u"\U0001F3E6"  # Bank
+        u"\U0001F3E8"  # Hotel
+        u"\U0001F4F4"  # Currency Exchange
+        u"\U0001F4F5"  # Heavy Dollar Sign
+        u"\U0001F4F6"  # Credit Card
+        u"\U0001F4F7"  # Banknote with Yen Sign
+        u"\U0001F4F8"  # Banknote with Euro Sign
+        u"\U0001F4F9"  # Banknote with Pound Sign
+        u"\U0001F4B4"  # Banknote with Yen Sign
+        u"\U0001F4B7"  # Banknote with Pound Sign
+        u"\U0001F4B6"  # Banknote with Euro Sign
+        u"\U0001F4C8"  # Chart Increasing with Yen Sign
+        u"\U0001F4C9"  # Chart Decreasing with Yen Sign
+        u"\U0001F4CA"  # Bar Chart with Yen Sign
+        u"\U0001F4CB"  # Bar Chart with Dollar Sign
+        u"\U0001F4CC"  # Bar Chart with Euro Sign
+        u"\U0001F4CD"  # Bar Chart with Pound Sign
+        u"\U0001F50E"  # Money-Mouth Face
+        u"\U0001F52A"  # Dollar Banknote
+        u"\U0001F52B"  # Euro Banknote
+        u"\U0001F4BB"  # Dollar Banknote with Wings
+        u"\U0001F4C1"  # Dollar Banknote with Coins
+        u"\U0001F4C2"  # Yen Banknote
+        u"\U0001F4C3"  # Dollar Banknote
+        u"\U0001F4C4"  # Euro Banknote
+        u"\U0001F4C5"  # Pound Banknote
+        u"\U0001F4C6"  # Money Bag
+        u"\U0001F4C7"  # Credit Card
+        u"\U0001F4C9"  # Chart Increasing
+        u"\U0001F4CA"  # Chart Decreasing
+        u"\U0001F4E6"  # Package
+        u"\U0001F4E7"  # E-Mail
+        u"\U0001F4E8"  # Incoming Envelope
+        u"\U0001F4E9"  # Envelope with Downwards Arrow Above
+        u"\U0001F4EA"  # Closed Mailbox with Lowered Flag
+        u"\U0001F4EB"  # Closed Mailbox with Raised Flag
+        u"\U0001F4EC"  # Open Mailbox with Raised Flag
+        u"\U0001F4ED"  # Open Mailbox with Lowered Flag
+        u"\U0001F4EE"  # Postbox
+        u"\U0001F4EF"  # Postal Horn
+        u"\U0001F4DC"  # Scroll
+        u"\U0001F4DD"  # Page with Curl
+        u"\U0001F4D6"  # Money with Wings
+        u"\U0001F4E2"  # Loudspeaker
+        u"\U0001F4E3"  # Right Speaker
+        u"\U0001F4E4"  # Right Speaker with One Sound Wave
+        u"\U0001F4E5"  # Right Speaker with Three Sound Waves
+        u"\U0001F4E6"  # Bullhorn
+        u"\U0001F4E7"  # Megaphone
+        u"\U0001F4E8"  # Postal Horn
+        u"\U0001F4E9"  # Bell
+        u"\U0001F4EA"  # Bell with Slash
+        u"\U0001F4EB"  # Bookmark
+        u"\U0001F4EC"  # Link Symbol
+        u"\U0001F4ED"  # Paperclip
+        u"\U0001F4EE"  # Linked Paperclips
+        u"\U0001F4EF"  # Black Pushpin
+        u"\U0001F4F0"  # White Pushpin
+        u"\U0001F4F1"  # Round Pushpin
+        u"\U0001F4F2"  # Triangular Pushpin
+        u"\U0001F4F3"  # Bookmark Tabs
+        u"\U0001F4F4"  # Ledger
+        u"\U0001F4F5"  # Notebook
+        u"\U0001F4F6"  # Notebook with Decorative Cover
+        u"\U0001F4F7"  # Closed Book
+        u"\U0001F4F8"  # Open Book
+        u"\U0001F4F9"  # Green Book
+        u"\U0001F4FA"  # Blue Book
+        u"\U0001F4FB"  # Orange Book
+        u"\U0001F4FC"  # Books
+        u"\U0001F4FD"  # Name Badge
+        u"\U0001F4FE"  # Scroll
+        u"\U0001F4FF"  # Memo
+        u"\U0001F52A"  # Closed Book
+        u"\U0001F52B"  # Open Book
+        u"\U0001F573"  # Newspaper
+        u"\U0001F58A"  # Money Bag
+        u"\U0001F58B"  # Currency Exchange
+        u"\U0001F58C"  # Heavy Dollar Sign
+        u"\U0001F58D"  # Credit Card
+        u"\U0001F58E"  # Dollar Banknote
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.findall(text)
 
 def analyze_sentiment(text):
     # Use TextBlob for polarity analysis
@@ -53,6 +173,9 @@ def classify_emotion(text):
             break
     
     return detected_emotion
+
+ 
+
 class Ui_OtherWindow(object):
     def setupUi(self, OtherWindow):
         OtherWindow.setObjectName("OtherWindow")
@@ -202,6 +325,7 @@ class Ui_OtherWindow(object):
                 for index, row in df.iterrows():
                     text = row['Tweets']  # Assuming 'Tweets' is the column name with the text data
 
+    
                     current_row_count = self.tableWidget.rowCount()
                     self.tableWidget.insertRow(current_row_count)
 
@@ -268,15 +392,21 @@ class Ui_OtherWindow(object):
         self.pushButton_2.clicked.connect(self.clearPlainText)
         
         self.pushButton_3.clicked.connect(self.updateTextInTable)
-
         
+ 
     def updateTextInTable(self):
-        # Get the text from the plain text edit widget
+
         text = self.plainTextEdit.toPlainText()
 
         current_row_count = self.tableWidget.rowCount()
         self.tableWidget.insertRow(current_row_count)
-
+        # Preprocess the text
+        text = preprocess_text(text)
+        # Detect emojis in the preprocessed text
+        emojis = detect_emojis(text)
+        # Append detected emojis to the preprocessed text
+        text_with_emojis = f"{text} {' '.join(emojis)}"
+    
         # Perform sentiment analysis using TextBlob
         blob = TextBlob(text)
         sentiment_score = blob.sentiment.polarity
@@ -284,7 +414,6 @@ class Ui_OtherWindow(object):
         # Classify the intensity level
         intensity = classify_intensity(sentiment_score)
         emotion = classify_emotion(text)
-
 
         # Set the sentiment result in the first row of the table (row 0, column 1)
         sentiment_item = QtWidgets.QTableWidgetItem(analyze_sentiment(text))
