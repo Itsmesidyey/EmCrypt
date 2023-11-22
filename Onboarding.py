1b43539697e1b499e0dc0a4b39021ee1e61988a1
lyndonjeff <jefflyndonebue@gmail.com>
2023-11-22 14:25:04 +0800

    start
    yey

--- Onboarding.py
+++ Onboarding.py
@@ -121,7 +121,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Continue >"))
+        self.pushButton.setText(_translate("MainWindow", "Start >"))
 import design
 
 
